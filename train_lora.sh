5f95b2242828ebc2239e692e8eb0aa98942516b2
Nick Zhang <ftdzh1994@gmail.com>
2024-03-04 21:04:19 +0800

    lora train code update

--- train_lora.sh
+++ train_lora.sh
@@ -1,9 +1,10 @@
-CUDA_VISIBLE_DEVICES=2,3 python3 train_lora.py \
+CUDA_VISIBLE_DEVICES=2,3 nohup python3 train_lora.py \
 --train_args_json chatGLM_6B_LoRA.json \
---model_name_or_path ./chatglm2-6b \
+--model_name_or_path ../models/chatglm2 \
 --train_data_path data/spo_0.jsonl \
 --eval_data_path data/spo_1.jsonl \
 --lora_rank 16 \
 -lora_alpha 64 \
 --lora_dropout 0.1 \
---compute_dtype fp32
+--compute_dtype fp32 \
+> chatglm_lora.log 2>&1 &
