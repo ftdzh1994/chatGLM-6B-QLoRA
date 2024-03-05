1e3701f8bd9df0e5ff9a3f5d144e52959b429a6c
root <root@bms-ved4z3.novalocal>
2024-03-05 14:22:04 +0800

    train code

--- train_lora.sh
+++ train_lora.sh
@@ -4,7 +4,7 @@ CUDA_VISIBLE_DEVICES=2,3 nohup python3 train_lora.py \
 --train_data_path data/spo_0.jsonl \
 --eval_data_path data/spo_1.jsonl \
 --lora_rank 16 \
--lora_alpha 64 \
+--lora_alpha 64 \
 --lora_dropout 0.1 \
 --compute_dtype fp32 \
 > chatglm_lora.log 2>&1 &
